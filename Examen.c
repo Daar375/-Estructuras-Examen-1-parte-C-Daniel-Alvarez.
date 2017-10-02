2422d662d15d60781bc05d311186ce2025532f8d
Daar375 <Daar375@gmail.com>
2017-10-02 11:34:41 -0600

    Update Examen.c

--- Examen.c
+++ Examen.c
@@ -40,7 +40,37 @@ Nodo  * getNodo(Nodo * Cabeza, int numNodo){
         return NodoIterador;
 }
 
-
+void reordenar(Nodo *  Cabeza1,Nodo *  Cabeza2){
+    Nodo * NodoTemp1 = Cabeza1;
+    Nodo * NodoTemp2 = Cabeza2;
+    int count = 0;
+    while(count!=5){
+        NodoTemp2=NodoTemp2->siguiente;
+        NodoTemp1=NodoTemp1->siguiente;
+        count++;
+    }
+    NodoTemp2->siguiente=NodoTemp1;
+    count=0;
+    
+    NodoTemp1 = Cabeza1;
+    NodoTemp2 = Cabeza2;
+    while(count!=4){
+        NodoTemp1=NodoTemp1->siguiente;
+        count++;
+    }
+    NodoTemp1->siguiente=Cabeza2;
+    
+    count=0;
+    NodoTemp1 = Cabeza1;
+    NodoTemp2 = Cabeza2;
+    while(count!=4){
+        NodoTemp1=NodoTemp1->siguiente;
+        count++;
+    }
+    NodoTemp1->siguiente=Cabeza2;
+    count=0;
+    
+}
 
 void print(Nodo *  Cabeza){
     Nodo * NodoTemp = Cabeza;
@@ -55,14 +85,33 @@ void print(Nodo *  Cabeza){
 int main()
 {   
     
-    Nodo * CabezaLista1 = malloc(sizeof(Nodo));
-    CabezaLista1 -> dato = 6;
+    Nodo * L1 = malloc(sizeof(Nodo));
+    L1 -> dato = 6;
 
-    agregarFinal(CabezaLista1,11);
-    agregarFinal(CabezaLista1,12);
-    agregarFinal(CabezaLista1,13);
+    agregarFinal(L1,1);
+    agregarFinal(L1,2);
+    agregarFinal(L1,3);
+    agregarFinal(L1,4);
+    agregarFinal(L1,8);
+    Nodo * L2 = malloc(sizeof(Nodo));
+    L2 -> dato = 9;
 
-   print(CabezaLista1);
-    printf("Nodo:%d",getNodo(CabezaLista1,10));
+    agregarFinal(L2,10);
+    agregarFinal(L2,11);
+    agregarFinal(L2,12);
+    agregarFinal(L2,13);
+    agregarFinal(L2,14);
+    print(L1);
+    printf("\n");
+    print(L2);
+    printf("\n");
+
+    reordenar(L1,L2);
+    printf("\n");
+
+    print(L1);
+    printf("\n");
+    print(L2);
+
     return 0;
 }
